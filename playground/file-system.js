7fb55495b40dfa44d85d141abb81d58381963c7f
synergy2411 <synergy2411@outlook.com>
2019-03-09 16:35:08 +0530

    Login form

--- playground/file-system.js
+++ playground/file-system.js
@@ -1,7 +1,21 @@
+// const fs = require("fs");
+
+// const readStream = fs.createReadStream("functions.js");
+
+// const writeStream = fs.createWriteStream("contents.txt");
+
+// readStream.pipe(writeStream);
+
+
+
+//Transform Stream
+
 const fs = require("fs");
+
+const gzip = require("zlib").createGzip();
 
 const readStream = fs.createReadStream("functions.js");
 
-const writeStream = fs.createWriteStream("contents.txt");
+const writeStream = fs.createWriteStream("contents.zip");
 
-readStream.pipe(writeStream);
+readStream.pipe(gzip).pipe(writeStream);
